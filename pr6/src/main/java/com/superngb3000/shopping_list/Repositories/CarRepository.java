1f4bfa7392a90c6061c3eef691547e14cc676c79
superngb3000 <71119087+superngb3000@users.noreply.github.com>
2021-12-15 12:25:21 +0300

    Delete pr6 directory

--- pr6/src/main/java/com/superngb3000/shopping_list/Repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.superngb3000.shopping_list.Repositories;
-
-import com.superngb3000.shopping_list.Entities.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
